5f7107adc369a3eb63405c37bede34de0261f371
jQuery <info@zavox.nl>
2016-01-03 18:18:45 +0100

    Zilean fixes

--- ElZilean/ElZilean/Program.cs
+++ ElZilean/ElZilean/Program.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LeagueSharp;
-using LeagueSharp.Common;
-using SharpDX;
-
-
-namespace ElZilean
+﻿namespace ElZilean
 {
+    using LeagueSharp.Common;
+
     internal class Program
     {
+        #region Methods
+
         private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += Zilean.Game_OnGameLoad;
         }
+
+        #endregion
     }
 }
